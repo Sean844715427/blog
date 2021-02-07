d7ca91ac920d493de8a292eef408d834728b213d
Sean844715427 <844715427@qq.com>
2021-02-08 03:03:05 +0800

    1111

--- model/category.js
+++ model/category.js
@@ -1,7 +1,16 @@
+const { json } = require('express')
 var db = require('../model/db')
 
 let menu = (req, res)=>{
+    console.log(body.req)
     var sql = `select * from menu`
+    db.connect(sql,(err,result)=>{
+    return result
+    })
+}
+
+let category = (req, res)=>{
+    var sql = `select * from article`
     db.connect(sql,(err,result)=>{
     return res.json(result)
     })
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var model = require('../model/category')
+// var model = require('../model/category')
+var db = require('../model/db')
 
+//获得文章
+router.post('/artcile', (req,res)=>{
+    console.log(req.body)
+    let sql = `select * from article `
+    db.connect(sql,(err,result)=>{
+        console.log(JSON.stringify(result))
+        res.send(result)
+    })
+})
 
-router.post('/', model.menu);
+//获取菜单
+router.post('/menu', (req,res)=>{
+    let sql = `select * from menu`
+    db.connect(sql,(err,result)=>{
+        console.log(JSON.stringify(result))
+        res.send(result)
+    })
+})
 
-module.exports = router;
+module.exports = router
