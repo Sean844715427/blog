ad53991ee844986608a61c60e3372d8c9df95225
SeangWang844715427 <1451912940@qq.com>
2021-02-08 16:19:11 +0800

    2222

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 // var model = require('../model/category')
 var db = require('../model/db')
+var request = require('request');
 
-//获得文章
+//文章
 router.post('/artcile', (req,res)=>{
     console.log(req.body)
     let sql = `select * from article `
@@ -13,13 +14,22 @@ router.post('/artcile', (req,res)=>{
     })
 })
 
-//获取菜单
+//菜单
 router.post('/menu', (req,res)=>{
     let sql = `select * from menu`
     db.connect(sql,(err,result)=>{
         console.log(JSON.stringify(result))
         res.send(result)
     })
+})
+
+//音乐类别
+router.post('/playlist/catlist', (req,res)=>{
+    request('https://musicapi.leanapp.cn/playlist/catlist', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        res.send(body)
+    }
+    });
 })
 
 module.exports = router
